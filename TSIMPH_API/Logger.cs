2c30d319f3891b841902391a167f32d65c824957
benjaminpants <maltbyfamily@gmail.com>
2022-04-10 02:25:08 -0700

    Initial commit

---
+++ TSIMPH_API/Logger.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSIMPH
+{
+    public static class Logger
+    {
+        public enum LogLevel
+        {
+            Debug,
+            Info,
+            Warn,
+            Error,
+            Fatal
+        }
+
+
+        public static void Log(string log, LogLevel level = LogLevel.Debug)
+        {
+            switch(level)
+            {
+
+                case LogLevel.Error:
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+
+                case LogLevel.Warn:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+
+                case LogLevel.Fatal:
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                    
+                case LogLevel.Debug:
+                    #if RELEASE
+                        return;
+                    #endif
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    break;
+
+                case LogLevel.Info:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+
+                default:
+                    break;
+
+            }
+            Console.WriteLine(log);
+            Console.ResetColor();
+        }
+    }
+}
---
+++ TSIMPH_API/Main.cs
@@ -0,0 +1,23 @@
+﻿using GmmlPatcher;
+using GmmlHooker;
+using UndertaleModLib;
+using UndertaleModLib.Models;
+using System.Linq;
+using System;
+using WysApi.Api;
+using System.IO;
+using UndertaleModLib.Decompiler;
+using System.Reflection;
+
+
+
+namespace TSIMPH
+{
+    public class GameMakerMod : IGameMakerMod
+    {
+        public void Load(int audioGroup, ModData currentmod)
+        {
+            
+        }
+    }
+}
