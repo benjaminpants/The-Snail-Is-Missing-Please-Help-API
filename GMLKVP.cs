7fc9115d246f00d6fc94b09cc5e75fce157eeb15
benjaminpants <maltbyfamily@gmail.com>
2023-09-12 18:46:23 -0700

    port to gs2ml

---
+++ GMLKVP.cs
@@ -0,0 +1,58 @@
+﻿using GmmlPatcher;
+using GmmlHooker;
+using UndertaleModLib;
+using UndertaleModLib.Models;
+using System.Linq;
+using System;
+using WysApi.Api;
+using System.IO;
+using UndertaleModLib.Decompiler;
+using System.Reflection;
+
+
+
+namespace TSIMPH
+{
+    public static class GMLKVP
+    {
+        public static Dictionary<string, string> DictionarizeGMLFolder(string gmlfolder)
+        {
+            Dictionary<string, string> Dict = new Dictionary<string, string>();
+
+            try
+            {
+                string[] infos = Directory.GetFiles(gmlfolder);
+
+                for (int i = 0; i < infos.Length; i++)
+                {
+                    FileInfo fo = new FileInfo(infos[i]);
+                    //fo.Name
+                    if (fo.Extension == ".gml")
+                    {
+                        Console.WriteLine("Reading File: " + fo.Name);
+                        Dict.Add(Path.GetFileNameWithoutExtension(fo.Name), File.ReadAllText(infos[i]));
+                        Patcher.AddFileToCache(0, infos[i]);
+                    }
+                }
+            }
+            catch
+            {
+                Logger.Log("Failed to read/open: " + gmlfolder, Logger.LogLevel.Error);
+                Logger.Log("Expect strange behavior or future crashes!", Logger.LogLevel.Error);
+                return new Dictionary<string, string>();
+            }
+
+            return Dict;
+        }
+
+        public static bool LoadGMLFolder(this Dictionary<string,string> GMLkvp, string gmlfolder)
+        {
+            Dictionary<string, string> dict = DictionarizeGMLFolder(gmlfolder);
+            foreach (KeyValuePair<string, string> kvp in dict)
+            {
+                GMLkvp.Add(kvp.Key, kvp.Value);
+            }
+            return dict.Count != 0;
+        }
+    }
+}
---
+++ gs2mlMod.cs
@@ -0,0 +1,40 @@
+﻿using GMHooker;
+using TSIMPH;
+using UndertaleModLib;
+
+namespace TSIMPH_API;
+
+public class TSIMPH_API
+{
+    public void Load(int audiogroup, UndertaleData data)
+    {
+        if(audiogroup != 0){
+            return;
+        }
+        
+        // Here, you can modify the data however you want. Go nuts!
+    }
+
+    /*public void LateLoad(int audioGroup, UndertaleData data,)
+    {
+        if (audioGroup != 0) return;
+        if (Conviences.SavePrefix == "")
+        {
+            data.Code.ByName("gml_Object_obj_persistent_Create_0").AppendGmlSafe(@"
+                    global.saveprefix = """"
+                ", data);
+            return;
+        }
+        Conviences.AddSavePrefix("_");
+        data.Strings.First(x => x.Content == "SaavoGame23-2.sav").Content = $"{Conviences.SavePrefix}SaavoGame23-2.sav";
+
+        data.Strings.First(x => x.Content == "SaveFileBackup-").Content = $"{Conviences.SavePrefix}SaveFileBackup-";
+
+        data.Strings.First(x => x.Content == "SaveFileBackup_alt_-").Content = $"{Conviences.SavePrefix}SaveFileBackup_alt_-";
+        data.Code.ByName("gml_Object_obj_persistent_Create_0").AppendGmlSafe($@"
+                    global.saveprefix = ""{Conviences.SavePrefix}""
+            ", data);
+    }*/
+
+
+}
